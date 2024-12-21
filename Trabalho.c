cd2808631f80ad6be7918241621fb50883535648
Eudes Nazário <eudesnazario178@gmail.com>
2024-12-21 13:11:24 +0000

    Dec 21, 2024, 10:11 AM

---
+++ Trabalho.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+struct tCLiente{
+    char nome[101]; // 100 caracteres + '\0'
+    char CPF[12]; // 11 digitos + '\0'
+    char telefone[15]; // 14 digitos + '\0'
+    int bonus;
+    float totCompras;
+    float tCompra; // ultima compra
+
+};
+
+int main(){
+    
+    return 0;
+}
